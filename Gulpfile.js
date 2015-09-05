771bf0b565ba7659ba31805cf8e5f0766cb01ee3
Arnoud van der Velden <vdvelden.a@gmail.com>
2015-09-05 17:08:43 +0200

    All files

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,15 +2,34 @@
  * Created by arnoudvandervelden on 27/07/15.
  */
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var concat = require('gulp-connect');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function(){
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
+        .pipe(gulp.dest('app/css/'));
 });
+
+gulp.task('scripts', function(){
+    gulp.src('scripts/**/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('app/scripts/'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/scripts/'))
+})
 
 //Watch task
 gulp.task('default', function(){
     gulp.watch('sass/**/*.scss', ['styles']);
-})
+    gulp.watch('scripts/**/*.js', ['scripts']);
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: '.',
+        livereload: true
+    })
+});
---
+++ Readme.md
@@ -0,0 +1,5 @@
+** Static site template Hans Raaijmakers Uitvaartzorg
+
+* Author:
+Arnoud van der Velden# hr-static_theme
+# hr-static_theme
