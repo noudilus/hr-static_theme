3d491cb9e0e6444d7b3dcf7415664b4da1e69ac8
Arnoud van der Velden <vdvelden.a@gmail.com>
2015-11-21 11:23:56 +0100

    A lot of stuff..

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,7 @@
-console.log('This is the Main.js file. It should be the 3rd and final file');
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 450) {
+        $('.navbar').addClass('smallnav');
+    } else {
+        $('.navbar').removeClass('smallnav');
+    }
+});
